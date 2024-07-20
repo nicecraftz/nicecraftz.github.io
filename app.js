a7a6269febb44065cda0c1f9010e88c04b235590
Alessandro Maria Calista <alessandromaria.calista@gmail.com>
2024-07-20 14:03:34 +0200

    Added last project worked on and added bootstrap css link

--- app.js
+++ app.js
@@ -1,15 +1,50 @@
-const test = () => {
-  const dynamicTestElement = document.getElementById("dynamic-test");
-  dynamicTestElement.innerHTML = "Hello World!";
+const username = "nicecraftz";
+const token = process.env.GH_TOKEN;
 
-  fetch("https://api.hypixel.net/skyblock/bazaar?api_key=abc")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      dynamicTestElement.innerHTML = JSON.stringify(data);
-      console.log(data);
-    });
+const getRepos = async (username, token) => {
+  const url = `https://api.github.com/users/${username}/repos`;
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `token ${token}`,
+    },
+  });
+  const data = await response.json();
+  return data;
 };
 
-test();
+const getEvents = async (username, token) => {
+  const url = `https://api.github.com/users/${username}/events`;
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `token ${token}`,
+    },
+  });
+  const data = await response.json();
+  return data;
+};
+
+const getLastProject = async (username, token) => {
+  try {
+    const repos = await getRepos(username, token);
+    const events = await getEvents(username, token);
+
+    let lastProject = null;
+    for (const event of events) {
+      if (
+        ["PushEvent", "PullRequestEvent", "IssuesEvent"].includes(event.type)
+      ) {
+        lastProject = event.repo.name;
+        break;
+      }
+    }
+
+    return lastProject;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
+  }
+};
+
+const lastProjectName = getLastProject(username, token);
+const paragaraphElement = document.getElementById("project");
+paragaraphElement.textContent = lastProjectName;
